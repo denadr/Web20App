11583a4554e1fbce22526877133493e2430eec98
Steffen-Laptop <Steffen-Laptop@139.30.150.160>
2016-05-27 16:28:56 +0200

    return uri

--- Web20/WebContent/Scripts/MusicAPI/spotify.js
+++ Web20/WebContent/Scripts/MusicAPI/spotify.js
@@ -3,9 +3,10 @@
  * searchQuery - name of album, artist or track,
  * numOfResults - number of results,
  * 
- * Results: -> iframes at "h2"
+ * Results: uris
  */
 function spotifySearch(type,searchQuery,numOfResults){
+	var result = "";
 	$.ajax(
 	{
 		url : "https://api.spotify.com/v1/search",
@@ -15,21 +16,24 @@ function spotifySearch(type,searchQuery,numOfResults){
 			type : type
 		},
 		success : function(response)
-		{
+		{	
 			for (var n = 0; n < numOfResults; n++){
+				var uri="";
 				switch (type){
 					case "track":
-						var uri = "https://embed.spotify.com/?uri="+response.tracks.items[n].uri;
+						uri = "https://embed.spotify.com/?uri="+response.tracks.items[n].uri;
 						break;
 					case "album":
-						var uri = "https://embed.spotify.com/?uri="+response.albums.items[n].uri;
+						uri = "https://embed.spotify.com/?uri="+response.albums.items[n].uri;
 						break;
 					case "artist":
-						var uri = "https://embed.spotify.com/?uri="+response.artists.items[n].uri;
+						uri = "https://embed.spotify.com/?uri="+response.artists.items[n].uri;
 						break;
 				}
-				$("h2").append('<iframe frameborder="0" allowTransparency="true" scrolling="no" width="250" height="80" src='+uri+'></iframe>');				
+				result+=uri+"\t"
+//				$("h2").append('<iframe frameborder="0" allowTransparency="true" scrolling="no" width="250" height="80" src='+uri+'></iframe>');				
 			}
 		}
 	});
+	return result;
 }
