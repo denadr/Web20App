fc54e40fea96d6db0f9cf41ba94d6cae66952f2c
Dennis Adrians <dennisadrians@t-online.de>
2016-06-03 12:46:31 +0200

    example for connecting code behind with view page

---
+++ Web20/WebContent/CodeBehind/search.js
@@ -0,0 +1,12 @@
+function search_button_click()
+{
+	// Collect parameters from UI
+	var query = $("#searchQuery").val();
+	var maxResults = 10;
+	
+	// TODO: Call API functionality with data and update UI
+	alert("Search for " + query);
+//	deezerSearch('track', query, maxResults);
+//	soundcloudSearch('tracks', query, maxResults);
+//	spotifySearch('track', query, maxResults);
+}
