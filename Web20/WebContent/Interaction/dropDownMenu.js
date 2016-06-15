9eefb32109760417f62bf4bb366bcbd94fb6ef27
ln047 <ln047@uni-rostock.de>
2016-06-15 14:31:32 +0200

    Minor Changes

--- Web20/WebContent/Interaction/dropDownMenu.js
+++ Web20/WebContent/Interaction/dropDownMenu.js
@@ -29,24 +29,6 @@ $(document).ready(function () {
     });
 });
 
-//$(document).ready(function () {
-//    togglescroll()
-//    $(".supplier_soundcloud").click(function () {
-//        $(".widgets_soundcloud").fadeToggle(500);
-//        $(".spotify_result_button").toggleClass("soundcloud-animate");
-//        $("body").toggleClass("noscroll");
-//    });
-//});
-//
-//$(document).ready(function () {
-//    togglescroll()
-//    $(".supplier_deezer").click(function () {
-//        $(".widgets_deezer").fadeToggle(500);
-//        $(".spotify_result_button").toggleClass("deezer-animate");
-//        $("body").toggleClass("noscroll");
-//    });
-//});
-
 // PUSH ESC KEY TO EXIT
 
 $(document).keydown(function(e) {
@@ -60,19 +42,3 @@ $(document).keydown(function(e) {
         $("body").removeClass("noscroll");
     }
 });
-
-//$(document).keydown(function(e) {
-//    if (e.keyCode == 27) {
-//        $(".widgets_soundcloud").fadeOut(500);
-//        $(".soundcloud_result_button").removeClass("soundcloud-animate");
-//        $("body").removeClass("noscroll");
-//    }
-//});
-//
-//$(document).keydown(function(e) {
-//    if (e.keyCode == 27) {
-//        $(".widgets_deezer").fadeOut(500);
-//        $(".deezer_result_button").removeClass("deezer-animate");
-//        $("body").removeClass("noscroll");
-//    }
-//});
