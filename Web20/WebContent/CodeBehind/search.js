1fba9e56e3850f7d5b048ee8bb215ca6772e31a0
Dennis Adrians <dennisadrians@t-online.de>
2016-07-03 17:28:35 +0200

    proof of concept for react usage on search.html

--- Web20/WebContent/CodeBehind/search.js
+++ Web20/WebContent/CodeBehind/search.js
@@ -1,51 +1,38 @@
-require.config(
+var HelloWorld = React.createClass(
 {
-	paths: 
-	{
-        'react': 'Interaction/react',
+    render : function () 
+    {
+    	return(<h1>Hello, world!</h1>);
     }
 });
 
-var React = null;
-var ReactDOM = null;
-require(['/Web20/Interaction/react.js'], function(react)
-	{
-		alert("REACT:" + react);
-		React = react;
-	});
-require(['/Web20/Interaction/react-dom.js'], function(reactDom)
-	{
-		alert("REACTDOM:" + reactDom);
-		ReactDOM = reactDom;
-	});
-require(['/Web20/Interaction/myreact.js'], function(myreact)
-		{
-			alert("MYREACT:" + myreact);
-		});
-
 var search_button_click = function ()
 {
+	ReactDOM.render(<HelloWorld/>, document.getElementById('results'));
 	// Collect parameters from UI
-	var query = $("#searchQuery").val();
+	var query = $('#searchQuery').val();
 	var maxResults = 10;
-	
+	alert('search_button_click: ' + query);
+
 	// TODO: Call API functionality with data and update UI
 	deezerSearch('track', query, maxResults, function (result)
 	{
-		//alert(result);
+		alert(result);
 	});
 	soundcloudSearch('tracks', query, maxResults, function (result)
 	{
-		//alert(result);
+		alert(result);
 	});
 	spotifySearch('track', query, maxResults, function (result)
 	{
 		alert(result);
-		for (var n = 0; n < result.length; n++)
-		{
-			alert('debug1');
-			ReactDOM.render(React.createElement(WidgetBox, {uri: result[n]}), document.getElementById("results"));
-			alert('debug3');
-		}
+//		for (var n = 0; n < result.length; n++)
+//		{
+//			alert('debug1');
+//			ReactDOM.render(React.createElement(WidgetBox, {uri: result[n]}), document.getElementById("results"));
+//			alert('debug3');
+//		}
 	});
 }
+
+document.getElementById('search_button').addEventListener('click', search_button_click);
