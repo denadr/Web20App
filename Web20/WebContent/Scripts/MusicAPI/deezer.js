73e061616dc5b0a993d94a28a892adb5f2a4cb3a
Dennis Adrians <dennisadrians@t-online.de>
2016-07-04 10:48:30 +0200

    deezer api calls use https now

--- Web20/WebContent/Scripts/MusicAPI/deezer.js
+++ Web20/WebContent/Scripts/MusicAPI/deezer.js
@@ -10,14 +10,14 @@ function deezerSearch(type,searchQuery,numOfResults, callback){
 
 	DZ.init({
 		appId  : '180042',
-		channelUrl : 'http://localhost:8080/DynamicTest/deezer.html',
+		channelUrl : 'https://localhost:8080/DynamicTest/deezer.html',
 	});
 	
 	DZ.api('/search?q='+type+':'+searchQuery, function(json){
 		var result = [];
 		var uri="";
 		for (var i=0; i<numOfResults ; i++){
-			uri = "http://www.deezer.com/plugins/player?autoplay=false&playlist=false&width=700&height=80&cover=true&type=tracks&id="+json.data[i].id+"&title=&app_id=undefined";					
+			uri = "https://www.deezer.com/plugins/player?autoplay=false&playlist=false&width=700&height=80&cover=true&type=tracks&id="+json.data[i].id+"&title=&app_id=undefined";					
 //			$("h3").append('<iframe frameborder="0" allowTransparency="true" scrolling="no" width="250" height="80" src='+uri+'></iframe>')
 			result[i]=uri;
 		}
