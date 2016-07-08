15d651eacd6f69c609d8bd96550b2106984bb41c
Dennis Adrians <dennisadrians@t-online.de>
2016-07-08 15:22:52 +0200

    first working drop down 'add' button for music widgets

--- Web20/WebContent/CodeBehind/search.js
+++ Web20/WebContent/CodeBehind/search.js
@@ -8,9 +8,66 @@ var HelloWorld = React.createClass(
 
 var WidgetFrame = React.createClass(
 {
-	render : function ()
+	getInitialState : function ()
 	{
-		return(<iframe frameborder="0" allowTransparency="true" scrolling="no" width="250" height="80" src={this.props.uri}></iframe>);
+		return { clicked : false };
+	},
+	
+	dropDown : function ()
+	{
+		this.setState({ clicked : !this.state.clicked });
+	},
+	
+	render : function ()
+	{		
+		var mainCss = 
+		{
+			float : 'right',
+	    	position : 'relative',
+	    	display : 'inline-block'
+		};
+		
+		var buttonCss =
+		{
+			backgroundColor : '#4CAF50',
+	    	color : 'white',
+	    	padding : 16,
+	    	fontSize : 16,
+			border : 'none',
+	    	cursor : 'pointer'
+		};
+		
+		var contentCss =
+		{
+			display : this.state.clicked ? 'block' : 'none',
+	    	position : 'absolute',
+	    	backgroundColor : '#f9f9f9',
+	    	minWidth : 160,
+	    	overflow : 'auto',
+	    	boxShadow : '0px 8px 16px 0px rgba(0,0,0,0.2)',
+	    	right : 0
+		};
+		
+		var optionCss =
+		{
+			color : 'black',
+	    	padding : '12px 16px',
+	    	textDecoration : 'none',
+	    	display : 'block'
+		};
+		
+		return(
+				<div>
+					<iframe frameborder="0" allowTransparency="true" scrolling="no" width="250" height="80" src={this.props.uri}></iframe>
+					<div style={mainCss}>
+						<button style={buttonCss} onClick={this.dropDown}>Dropdown</button>
+						<div style={contentCss}>
+		    				<a style={optionCss} href="#home">Home</a>
+		    				<a style={optionCss} href="#help">Help</a>
+		    			</div>
+		    		</div>
+				</div>
+			);
 	}
 });
 //
